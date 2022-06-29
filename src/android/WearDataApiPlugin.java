51afc2e6daadf803f47bb79e595d09a04d0ebdad
Michael Pleger <29815455+plegermi@users.noreply.github.com>
2022-06-29 15:41:40 +0200

    Squashed patches for RehaGoal
    - refactored putDataItem method
      - removed JsonConverter to convert Object into HashMaps
      - putData will put an incoming string (json object) into the data property instead
      - added msgID with random id (this way, every putRequest will be unique and no deleteItem is neccessary
      - added the .setUrgent flag for instant transmission
    - fixed NullPointerException when Service is connected, but the application has not been started yet
      - service will call WearDataPluginSingleton.onServiceStarted only if PluginSingleton is defined (not null)
    - removed finalize calls from Singletons
      - removed finalize functions for java gc cleanup
      - added logging for debugging purpose
    - added missing import statement
    - import missing status package & adjusted status log msg
    - refactored Service
      - removed singleton for service
      - changed plugin to use data api directly

--- src/android/WearDataApiPlugin.java
+++ src/android/WearDataApiPlugin.java
@@ -3,18 +3,27 @@ package us.m4rc.cordova.androidwear.dataapi;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultTransform;
+import com.google.android.gms.common.api.ResultCallbacks;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataItemBuffer;
+import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaDialogsHelper;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
@@ -26,13 +35,16 @@ import org.json.JSONObject;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 import static com.google.android.gms.wearable.DataApi.FILTER_LITERAL;
 
 public class WearDataApiPlugin extends CordovaPlugin
-    implements DataApi.DataListener {
+    implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks  {
 
-  public static WearDataApiPlugin WearDataApiPluginSingleton = null;
+  static WearDataApiPlugin WearDataApiPluginSingleton = null;
+  private GoogleApiClient mGoogleApiClient;
+
 
   private final String TAG = WearDataApiPlugin.class.getSimpleName();
   private final String[] ACTIONS = {
@@ -48,6 +60,17 @@ public class WearDataApiPlugin extends CordovaPlugin
   // for queueing actions while api is starting up
   private Queue<ExecuteAction> queuedActions = new LinkedList<ExecuteAction>();
 
+  @Override
+  public void onConnected(@Nullable Bundle bundle) {
+      Log.d(TAG, "Google API Client connected");
+      onServiceStarted();
+  }
+
+  @Override
+  public void onConnectionSuspended(int i) {
+
+  }
+
 
   private class ExecuteAction {
     public String action;
@@ -61,11 +84,7 @@ public class WearDataApiPlugin extends CordovaPlugin
     }
   }
 
-  private WearDataApiService api() {
-    return WearDataApiService.WearDataApiServiceSingleton;
-  }
-
-  public void onServiceStarted() {
+  private void onServiceStarted() {
     // run all queued actions
     while (queuedActions.size() > 0) {
       ExecuteAction ea = queuedActions.remove();
@@ -86,6 +105,18 @@ public class WearDataApiPlugin extends CordovaPlugin
     Activity context = cordova.getActivity();
     WearDataApiPluginSingleton = this;
     context.startService(new Intent(context, WearDataApiService.class));
+
+    // set up the Google API client
+    if (mGoogleApiClient==null) {
+      mGoogleApiClient = new GoogleApiClient.Builder(cordova.getActivity())
+              .addApi(Wearable.API)
+              .addConnectionCallbacks(this)
+              .build();
+    }
+    if (!mGoogleApiClient.isConnected()) {
+      mGoogleApiClient.connect();
+    }
+    Log.d(TAG, "WearDataApiService started.");
   }
 
   @Override
@@ -94,11 +125,12 @@ public class WearDataApiPlugin extends CordovaPlugin
     WearDataApiPluginSingleton = null; // if we are called gracefully
   }
 
-  @Override
-  protected void finalize() throws Throwable {
-    super.finalize();
-    WearDataApiPluginSingleton = null; // when we are garbage collected
-  }
+
+   @Override
+   protected void finalize() throws Throwable {
+     super.finalize();
+     WearDataApiPluginSingleton = null; // when we are garbage collected
+   }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
@@ -107,7 +139,7 @@ public class WearDataApiPlugin extends CordovaPlugin
       return false;
     }
     ExecuteAction ea = new ExecuteAction(action,args,callbackContext);
-    if (this.api()!=null) {
+    if (mGoogleApiClient.isConnected()) {
       try {
         _execute(ea); // run immediately if already connected
       }
@@ -151,14 +183,36 @@ public class WearDataApiPlugin extends CordovaPlugin
       throw new JSONException("putDataItem error: invalid arguments");
     }
     PutDataMapRequest putDataMapReq = PutDataMapRequest.create(args.getString(0));
-    JsonConverter.jsonToDataMap((JSONObject)args.get(1), putDataMapReq.getDataMap());
-    this.api().putDataRequest(putDataMapReq.asPutDataRequest())
+    // Original: convert object with JsonConverter
+    //      JsonConverter.jsonToDataMap((JSONObject)args.get(1), putDataMapReq.getDataMap());
+
+    // Changed: put complete json in data property
+    DataMap response = putDataMapReq.getDataMap();
+    long id = new Random(System.currentTimeMillis()).nextLong();
+    response.putLong("msgID", id);
+    response.putString("data", (String) args.get(1));
+
+    // Changed: added setUrgent flag
+    this.putDataRequest(putDataMapReq.asPutDataRequest().setUrgent())
       .then(new ResultTransform<DataApi.DataItemResult, Result>() {
         @Override
         public PendingResult<Result> onSuccess(@NonNull DataApi.DataItemResult dataItemResult) {
           callbackContext.success();
           return null;
         }
+      })
+      .andFinally(new ResultCallbacks<Result>() {
+        //TODO: remove andFinally after successful testing
+        @Override
+        public void onSuccess(@NonNull Result r) {
+          // nothing to do.
+        }
+        @Override
+        public void onFailure(@NonNull Status status) {
+          // An API call failed.
+          Log.d(TAG, "putDataRequest failed! with status code: " + status.getStatusCode() + "and msg: " + status.getStatusMessage());
+        }
+
       });
   }
 
@@ -172,7 +226,7 @@ public class WearDataApiPlugin extends CordovaPlugin
       filterType = args.getInt(1);
     }
     if (cmd.equals("get")) {
-      this.api().getDataItems(uri, filterType).then(new ResultTransform<DataItemBuffer, Result>() {
+      this.getDataItems(uri, filterType).then(new ResultTransform<DataItemBuffer, Result>() {
         @Override
         public PendingResult<Result> onSuccess(@NonNull DataItemBuffer dataItems) {
           callbackContext.success(JsonConverter.dataItemBufferToJson(dataItems));
@@ -180,7 +234,7 @@ public class WearDataApiPlugin extends CordovaPlugin
         }
       });
     } else if (cmd.equals("delete")) {
-      this.api().deleteDataItems(uri, filterType).then(new ResultTransform<DataApi.DeleteDataItemsResult, Result>() {
+      this.deleteDataItems(uri, filterType).then(new ResultTransform<DataApi.DeleteDataItemsResult, Result>() {
         @Override
         public PendingResult<Result> onSuccess(@NonNull DataApi.DeleteDataItemsResult deleteResult) {
           JSONObject json = new JSONObject();
@@ -204,6 +258,7 @@ public class WearDataApiPlugin extends CordovaPlugin
     registeredListeners.add(callbackContext);
   }
 
+  @Override
   public void onDataChanged(DataEventBuffer dataEventBuffer) {
     JSONArray results = JsonConverter.dataEventBufferToJson(dataEventBuffer);
     for (CallbackContext registeredDataReceiver: registeredListeners) {
@@ -213,5 +268,18 @@ public class WearDataApiPlugin extends CordovaPlugin
       result.setKeepCallback(true);
       registeredDataReceiver.sendPluginResult(result);
     }
+  }
+
+  // methods below form a thin wrapper around the DataApi for use in the Plugin
+  private PendingResult<DataApi.DataItemResult> putDataRequest(PutDataRequest data) {
+    return Wearable.DataApi.putDataItem(mGoogleApiClient, data);
+  }
+
+  private PendingResult<DataApi.DeleteDataItemsResult> deleteDataItems(Uri uri, int filterType) {
+    return Wearable.DataApi.deleteDataItems(mGoogleApiClient, uri, filterType);
+  }
+
+  private PendingResult<DataItemBuffer> getDataItems(Uri uri, int filterType) {
+    return Wearable.DataApi.getDataItems(mGoogleApiClient, uri, filterType);
   }
 }
--- src/android/WearDataApiService.java
+++ src/android/WearDataApiService.java
@@ -1,32 +1,23 @@
 package us.m4rc.cordova.androidwear.dataapi;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataItemBuffer;
-import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
 
 public class WearDataApiService extends WearableListenerService
     implements GoogleApiClient.ConnectionCallbacks {
 
-  public static WearDataApiService WearDataApiServiceSingleton = null;
   private final String TAG = WearDataApiService.class.getSimpleName();
   private GoogleApiClient mGoogleApiClient;
 
   @Override
   public void onConnected(@Nullable Bundle bundle) {
     Log.d(TAG, "Google API Client connected");
-    if (WearDataApiPlugin.WearDataApiPluginSingleton != null) {
-      WearDataApiPlugin.WearDataApiPluginSingleton.onServiceStarted();
-    }
   }
 
   @Override
@@ -47,7 +38,6 @@ public class WearDataApiService extends WearableListenerService
       mGoogleApiClient.connect();
     }
     Log.d(TAG, "WearDataApiService started.");
-    WearDataApiServiceSingleton = this;
   }
 
   @Override
@@ -57,7 +47,6 @@ public class WearDataApiService extends WearableListenerService
       mGoogleApiClient.disconnect();
     }
     Log.d(TAG, "WearDataApiService shut down.");
-    WearDataApiServiceSingleton = null; // if we are shutdown gracefully
     super.onDestroy();
   }
 
@@ -73,22 +62,4 @@ public class WearDataApiService extends WearableListenerService
     }
   }
 
-  // methods below form a thin wrapper around the DataApi for use in the Plugin
-  public PendingResult<DataApi.DataItemResult> putDataRequest(PutDataRequest data) {
-    return Wearable.DataApi.putDataItem(mGoogleApiClient, data);
-  }
-
-  public PendingResult<DataApi.DeleteDataItemsResult> deleteDataItems(Uri uri, int filterType) {
-    return Wearable.DataApi.deleteDataItems(mGoogleApiClient, uri, filterType);
-  }
-
-  public PendingResult<DataItemBuffer> getDataItems(Uri uri, int filterType) {
-    return Wearable.DataApi.getDataItems(mGoogleApiClient, uri, filterType);
-  }
-
-  @Override
-  protected void finalize() throws Throwable {
-    super.finalize();
-    WearDataApiServiceSingleton = null; // when we are garbage collected
-  }
 }
